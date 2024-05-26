d98f194d0951d7e754208fdd2d2483c1ef595398
ahmedfrikal <ahmad.frikal@gmail.com>
2024-05-26 16:24:40 +0100

    Implemented Font Awesome icons in the application

--- spotify-clone-front/src/app/shared/font-awesome-icons.ts
+++ spotify-clone-front/src/app/shared/font-awesome-icons.ts
@@ -1,6 +1,8 @@
 import {IconDefinition} from "@fortawesome/fontawesome-svg-core";
-import {faCirclePlay} from "@fortawesome/free-solid-svg-icons";
+import {faCirclePlay, faHome, faSearch} from "@fortawesome/free-solid-svg-icons";
 
 export const fontAwesomeIcons:IconDefinition[] =[
-  faCirclePlay
+  faCirclePlay,
+  faHome,
+  faSearch
 ]
