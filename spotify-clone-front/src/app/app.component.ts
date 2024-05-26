52061360c5295a430713443c8b63921395f6ff0f
ahmedfrikal <ahmad.frikal@gmail.com>
2024-05-26 16:33:57 +0100

    Add link  library

--- spotify-clone-front/src/app/app.component.ts
+++ spotify-clone-front/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {fontAwesomeIcons} from "./shared/font-awesome-icons";
 import {NavigationComponent} from "./layout/navigation/navigation.component";
+import {LibraryComponent} from "./layout/library/library.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FontAwesomeModule, NavigationComponent],
+  imports: [RouterOutlet, FontAwesomeModule, NavigationComponent, LibraryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
