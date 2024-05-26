c6a3c23a8ba92e97fdac959d6f1ea93426b3f940
ahmedfrikal <ahmad.frikal@gmail.com>
2024-05-26 15:49:57 +0100

    configure fontAwesome

--- spotify-clone-front/src/app/app.component.ts
+++ spotify-clone-front/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'spotify-clone-front';
+
+  private faIconLibrary=inject(FaIconLibrary);
+
+  private initFontAwesome(){
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
 }
