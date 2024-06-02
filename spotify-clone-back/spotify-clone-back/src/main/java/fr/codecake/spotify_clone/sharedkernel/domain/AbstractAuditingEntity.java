d362cc569d381179d022ea5e7c1fac43632f0e4b
ahmedfrikal <ahmad.frikal@gmail.com>
2024-06-02 13:39:26 +0100

    create entity abstract

---
+++ spotify-clone-back/spotify-clone-back/src/main/java/fr/codecake/spotify_clone/sharedkernel/domain/AbstractAuditingEntity.java
@@ -0,0 +1,41 @@
+package fr.codecake.spotify_clone.sharedkernel.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractAuditingEntity <T> implements Serializable {
+
+    public abstract T getId();
+
+    @CreatedDate
+    @Column(name="created_date",updatable = false)
+    private Instant createdDate = Instant.now();
+
+    public Instant getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Instant createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Instant getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Instant lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    @LastModifiedDate
+    private Instant lastModifiedDate = Instant.now();
+}
